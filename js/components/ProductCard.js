464bae927bacc143da79bf0a6b0f66054a23e131
vankossss <ivan.zhukow@e-u.edu.ua>
2026-05-28 16:52:15 +0200

    Update ProductCard.js

--- js/components/ProductCard.js
+++ js/components/ProductCard.js
@@ -1,6 +1,6 @@
 export function ProductCard(product) {
     return `<div class="item-card" data-id="${product.id}">
-    <img src="${product.thumbnail}" alt="${product.title}" class="item-image" loading="lazy" decoding="async" style="width: 100%; height: auto;">
+    <img src="${product.thumbnail}" alt="${product.title}" class="item-image" loading="lazy" decoding="async">
     <div class="item-info">
         <span>${product.title}</span>
         <b>${product.price}$</b>
